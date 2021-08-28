756fa347baf3fad2f346e1e103a1ea1fc1f5af60
samarthpathak3 <freelancingonupwork@gmail.com>
2021-08-28 20:08:52 +0530

    initial committ

---
+++ js/custom.js
@@ -0,0 +1,120 @@
+/*************************
+       Owl Carousel
+*************************/
+jQuery(document).ready(function(){
+    var owlslider = jQuery("div.owl-carousel");
+    if (owlslider.length > 0) {
+      owlslider.each(function () {
+        var $this = $(this),
+          $items = ($this.data('items')) ? $this.data('items') : 1,
+          $loop = ($this.attr('data-loop')) ? $this.data('loop') : true,
+          $navdots = ($this.data('nav-dots')) ? $this.data('nav-dots') : false,
+          $navarrow = ($this.data('nav-arrow')) ? $this.data('nav-arrow') : false,
+          $autoplay = ($this.attr('data-autoplay')) ? $this.data('autoplay') : true,
+          $autospeed = ($this.attr('data-autospeed')) ? $this.data('autospeed') : 5000,
+          $smartspeed = ($this.attr('data-smartspeed')) ? $this.data('smartspeed') : 1000,
+          $autohgt = ($this.data('autoheight')) ? $this.data('autoheight') : false,
+          $space = ($this.attr('data-space')) ? $this.data('space') : 30,
+          $animateOut = ($this.attr('data-animateOut')) ? $this.data('animateOut') : false;
+
+        $(this).owlCarousel({
+          loop: $loop,
+          items: $items,
+          responsive: {
+            0: {
+              items: $this.data('xx-items') ? $this.data('xx-items') : 1
+            },
+            480: {
+              items: $this.data('xs-items') ? $this.data('xs-items') : 1
+            },
+            768: {
+              items: $this.data('sm-items') ? $this.data('sm-items') : 2
+            },
+            980: {
+              items: $this.data('md-items') ? $this.data('md-items') : 3
+            },
+            1200: {
+              items: $items
+            }
+          },
+          dots: $navdots,
+          autoplayTimeout: $autospeed,
+          smartSpeed: $smartspeed,
+          autoHeight: $autohgt,
+          margin: $space,
+          nav: $navarrow,
+          navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+          autoplay: $autoplay,
+          autoplayHoverPause: true
+        });
+      });
+    }
+});
+
+  /*************************
+        Swiper slider
+*************************/
+jQuery(document).ready(function(){
+  var siperslider = jQuery(".swiper-container");
+  if (siperslider.length > 0) {
+    var swiperAnimation = new SwiperAnimation();
+        var swiper = new Swiper(".swiper-container", {
+          init : true,
+          direction: "horizontal",
+          effect: "slide",
+          loop: true,
+          keyboard: {
+            enabled: true,
+            onlyInViewport: true
+          },
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+          on: {
+            init: function() {
+              swiperAnimation.init(this).animate();
+            },
+            slideChange: function() {
+              swiperAnimation.init(this).animate();
+            }
+          }
+        });
+    }
+});
+
+/*************************
+      Shuffle
+  *************************/
+jQuery(document).ready(function(){
+   if ($('.my-shuffle-container')) {
+    var Shuffle = window.Shuffle;
+      var element = document.querySelector('.my-shuffle-container');
+      var sizer = element.querySelector('.my-sizer-element');
+      var shuffleInstance = new Shuffle(element, {
+        itemSelector: '.grid-item',
+        sizer: sizer, // could also be a selector: '.my-sizer-element'
+        speed: 700,
+        columnThreshold: 0
+      });
+      jQuery(document).ready(function(){
+        jQuery(".btn-filter").on( 'click', function(){
+          var data_group = jQuery(this).attr('data-group');
+          if( data_group != 'all' ){
+            shuffleInstance.filter([data_group]);
+          } else {
+            shuffleInstance.filter();
+          }
+        });
+        $(".filters-group .btn-filter").on( 'click', function(){
+            $(".filters-group .btn-filter").removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+  }
+});
